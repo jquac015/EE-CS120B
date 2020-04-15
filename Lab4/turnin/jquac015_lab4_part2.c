f71ff5c90a506356672de1e9e0340dbe955c3739
Justin Quach <jquac015@wch136-11.cs.ucr.edu>
2020-04-14 21:47:50 -0700

    Lab 4

--- Lab4/turnin/jquac015_lab4_part2.c
+++ Lab4/turnin/jquac015_lab4_part2.c
@@ -22,9 +22,9 @@ void CounterSM(){
 		case STANDBY:
 			if((PINA == 0x03)){
 				Counter_State = RESET;
-			}else if((PINA&0x02) && (PINC>0x00)){
+			}else if((PINA&0x02) && (PINC>0)){
 				Counter_State = DEC;
-			}else if((PINA&0x01) && (PINC<0x09)){
+			}else if((PINA&0x01) && (PINC<9)){
 				Counter_State = INC;
 			}else{
 				Counter_State = STANDBY;
